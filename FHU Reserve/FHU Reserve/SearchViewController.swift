1019b92aea70ef58def074d2632e76e993874610
Jeremy Beard <jeremy.beard96@gmail.com>
2017-11-13 16:24:52 -0600

    Add files via upload

---
+++ FHU Reserve/FHU Reserve/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  FHU Reserve
+//
+//  Created by Jeremy Beard on 11/7/17.
+//  Copyright © 2017 Freed Hardeman University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var capacityPicker: UISegmentedControl!
+    @IBOutlet weak var durationPicker: UISegmentedControl!
+    @IBOutlet weak var datePicker: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
