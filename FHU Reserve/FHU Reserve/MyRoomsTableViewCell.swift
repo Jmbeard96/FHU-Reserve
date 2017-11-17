990f4d9b0d69a57114df0636c9998a0bfae5bc41
Jeremy Beard <jeremy.beard96@gmail.com>
2017-11-17 00:30:37 -0600

    11-17

---
+++ FHU Reserve/FHU Reserve/MyRoomsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MyRoomsTableViewCell.swift
+//  FHU Reserve
+//
+//  Created by Jeremy Beard on 11/17/17.
+//  Copyright © 2017 Freed Hardeman University. All rights reserved.
+//
+
+import UIKit
+
+class MyRoomsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var roomImage: UIImageView!
+    @IBOutlet weak var roomNumber: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- FHU Reserve/FHU Reserve/SecondViewController.swift
+++ FHU Reserve/FHU Reserve/SecondViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myRooms = RoomSet.roomsForTimes
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return myRooms.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myRooms[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReservedRoomCell", for: indexPath)
+        
+        if let reservedRoomCell = cell as? MyRoomsTableViewCell{
+            reservedRoomCell.roomNumber.text = myRooms[indexPath.section][indexPath.row].roomNumber?.description
+            if let imageName = myRooms[indexPath.section][indexPath.row].imageName{
+                reservedRoomCell.roomImage?.image = UIImage(named: imageName)
+            }
+            else{
+                reservedRoomCell.roomImage?.image = nil
+            }
+            
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        switch section{
+        case 0: return "1:00pm - 2:00pm"
+        case 1: return "2:00pm - 3:00pm"
+        case 2: return "3:00pm - 4:00pm"
+        default: return "TBA"
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        navigationItem.title = "My Rooms"
     }
 
     override func didReceiveMemoryWarning() {
