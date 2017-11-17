e31c9572717cf7b4a4ff40e8fd2d2c4ffb43c496
Keith Mattix II <keithmattix2@gmail.com>
2017-11-17 02:33:57 -0600

    Configure the calendar with the correct colors and functionality

--- FHU Reserve/FHU Reserve/SearchViewController.swift
+++ FHU Reserve/FHU Reserve/SearchViewController.swift
@@ -16,15 +16,53 @@ class SearchViewController: UIViewController, JBDatePickerViewDelegate {
     @IBOutlet weak var capacityPicker: UISegmentedControl!
     @IBOutlet weak var durationPicker: UISegmentedControl!
     
+    @IBOutlet weak var dateLabel: UILabel!
+    var primaryColor = UIColor.init(red: 102/255.0, green: 20/255.0, blue: 36/255.0, alpha: 1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         calendar.delegate = self
-        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateLabel.text = "Date - " + dateFormatter.string(from: calendar.selectedDateView.date!)
     }
     func didSelectDay(_ dayView: JBDatePickerDayView) {
-        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateLabel.text = "Date - " + dateFormatter.string(from: dayView.date!)
     }
+    
+    var colorForWeekDaysViewBackground: UIColor { return primaryColor }
+    
+    var colorForSelectionCircleForOtherDate: UIColor { return primaryColor }
+    
+    var colorForSelectionCircleForToday: UIColor { return primaryColor }
+    
+    func shouldAllowSelectionOfDay(_ date: Date?) -> Bool {
+        let currentDate = Date()
+        //this code example disables selection for dates older then today
+        guard let date = date else {return true}
+        let comparison = NSCalendar.current.compare(date, to: currentDate, toGranularity: .day)
+        
+        if comparison == .orderedAscending {
+            return false
+        }
+        
+        // this part prevents selecting dates further than two weeks in the future
+        var dateComponent = DateComponents()
+        dateComponent.day = 14 // Add 2 weeks
+        let twoWeeksFromNow = Calendar.current.date(byAdding: dateComponent, to: currentDate)
+        let twoWeeksComparison = NSCalendar.current.compare(date, to: twoWeeksFromNow!, toGranularity: .day)
+        if twoWeeksComparison == .orderedDescending {
+            return false
+        }
+        return true
+    }
+
+
 }
 
 
